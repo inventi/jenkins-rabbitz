87a325655cf4ad3db1ef9d093c9dac37b5e71d0f
Zilvinas Kybartas <zilvinas@inventi.lt>
2012-10-29 12:37:35 +0200

    Add test definition

--- src/test/clojure/karotz/test/core.clj
+++ src/test/clojure/karotz/test/core.clj
@@ -2,18 +2,20 @@
   (:use [karotz.core])
   (:use [clojure.test]))
 
-(testing "Should test error"
-         (is (error? "ERROR"))
-         (is (error? "NOT_CONNECTED")))
+(deftest test-error 
+         (testing "Should test error"
+                  (is (error? "ERROR"))
+                  (is (error? "NOT_CONNECTED"))))
 
-(testing "Signin should return valid id"
-         (is (= "12345"
-                (with-redefs [valid-id? (constantly true)]
-                             (sign-in {:interactive-id "12345"}))))
-         (is (= "new-id"
-                (with-redefs [valid-id? (constantly false)
-                              karotz-request (constantly "new-id")]
-                             (sign-in {:interactive-id "old-id" :secret "123"})))))
+(deftest test-sign-in
+         (testing "Signin should return valid id"
+                  (is (= "12345"
+                         (with-redefs [valid-id? (constantly true)]
+                                      (sign-in {:interactive-id "12345"}))))
+                  (is (= "new-id"
+                         (with-redefs [valid-id? (constantly false)
+                                       karotz-request (constantly "new-id")]
+                                      (sign-in {:interactive-id "old-id" :secret "123"}))))))
 
 
 
