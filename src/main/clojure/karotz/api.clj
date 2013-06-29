42d6b2c76774eea3a125d5b4144fd41862d1707d
Zilvinas Kybartas <zilvinas@inventi.lt>
2013-06-29 21:39:07 +0300

    Improve error handling and api

--- src/main/clojure/karotz/api.clj
+++ src/main/clojure/karotz/api.clj
@@ -1,6 +1,6 @@
 (ns karotz.api
   (:require [clojure.xml :as xml])
-  (:import java.io.IOException           
+  (:import java.io.IOException
            java.net.URLEncoder
            javax.crypto.Mac
            javax.crypto.spec.SecretKeySpec
@@ -9,8 +9,8 @@
 (def karotz-api "http://api.karotz.com/api/karotz/")
 
 (defn tag-content [tag content]
-  (first 
-    (for [x (xml-seq content) 
+  (first
+    (for [x (xml-seq content)
           :when (= tag (:tag x))]
       (first (:content x)))))
 
@@ -25,49 +25,53 @@
 (defn karotz-request
   ([token url]
     (karotz-request (str url "&interactiveid=" token)))
-  
-  ([url] 
-   (try (let [content (xml/parse (str karotz-api url))  
+
+  ([url]
+   (let [content (xml/parse (str karotz-api url))
          code (tag-content :code content)]
      (if (error? code)
        code
-       (tag-content :interactiveId content)))
-          (catch IOException e (str "ERROR " e)))))
+       (tag-content :interactiveId content)))))
 
 
 (defn sign-query [query secret]
-  (String. (Base64/encodeBase64 
+  (String. (Base64/encodeBase64
              (let [ mac (Mac/getInstance "HmacSHA1")]
-               (do 
+               (do
                  (.init mac (SecretKeySpec. (.getBytes secret) "HmacSHA1"))
                  (.doFinal mac (.getBytes query))))) "ASCII"))
 
 (defn login-url [api-key secret installid]
-    (let [query (str "apikey=" api-key 
-                     "&installid=" installid 
-                     "&once=" (str (.nextInt (java.util.Random.) 99999999)) 
+    (let [query (str "apikey=" api-key
+                     "&installid=" installid
+                     "&once=" (str (.nextInt (java.util.Random.) 99999999))
                      "&timestamp=" (long (/ (System/currentTimeMillis) 1000)))]
       (str "start?" query "&signature=" (URLEncoder/encode (sign-query query secret) "utf8"))))
 
 (defn valid-id? [token]
-    (let [response (karotz-request token "ears?left=10&right=-10&relative=true")]
-      (not (error? response))))
+  (try
+    (let [response (karotz-request token "ears?left=0&right=0&relative=true")]
+      (not (error? response)))
+    (catch IOException e false)))
 
-(defn sign-in 
-  "sign-ins to karotz with provided credentials. Data should be provided as map.
-  {:api-key <api-key> :secret <secret> 
-   :interactive-ids {<karotz-id> <last known interactive id>}}"  
+(defn sign-in
   ([[karotz-id token] credentials]
     (if (valid-id? token)
         token
         (let [{api-key :api-key secret :secret} credentials]
           (karotz-request (login-url api-key secret karotz-id))))))
 
-(defn move-ears [token]
-  (let [request-url "ears?left=20&right=-30&relative=true"]
-    (karotz-request token request-url)))
+(defn move-ears
+  ([token]
+   (move-ears token 20 -30))
+
+  ([token left right]
+   (let [request-url (str "ears?left=" left "&right=" right "&relative=true")]
+     (do
+       (println request-url)
+       (karotz-request token request-url)))))
 
 (defn say-out-loud [token media-url]
-  (let [request-url (str "multimedia?action=play&url=" 
+  (let [request-url (str "multimedia?action=play&url="
                        (URLEncoder/encode media-url))]
     (karotz-request token request-url)))
