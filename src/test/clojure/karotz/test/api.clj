b46f750b33d3e1acf0bd2a71b00942696aeb6065
Zilvinas Kybartas <zilvinas@inventi.lt>
2013-07-10 22:09:26 +0300

    Fix test

--- src/test/clojure/karotz/test/api.clj
+++ src/test/clojure/karotz/test/api.clj
@@ -3,7 +3,7 @@
   (:use [clojure.test])
   (:require [clojure.xml :as xml]))
 
-(deftest test-error 
+(deftest test-error
          (testing "Should test error"
                   (is (error? "ERROR some other text"))
                   (is (error? "NOT_CONNECTED some other text"))
@@ -14,23 +14,25 @@
 (deftest test-karotz-request
   (with-redefs [karotz-api ""]
     (testing "Parses correct response"
-             (is (= "INTERACTIVE-ID" 
+             (is (= "INTERACTIVE-ID"
                     (karotz-request "src/test/resources/ok-response.xml"))))
-  
+
   (testing "Parses erroneus response"
-	  (is (= "NOT_CONNECTED" 
+	  (is (= "NOT_CONNECTED"
 	         (karotz-request "src/test/resources/error-response.xml"))))
-  
+
   (testing "Returns io exception if it occurs"
-	  (is (= "ERROR java.io.IOException: problem"
+	  (is (= java.io.IOException
            (with-redefs [xml/parse (fn [a] (throw (java.io.IOException. "problem")))]
-             (karotz-request "no-such-file")))))
-  
+             (try
+               (karotz-request "no-such-file")
+               (catch java.io.IOException e (.getClass e)))))))
+
   (testing "Returns new interactive id"
-           (is (= "INTERACTIVE-ID-WITH-PARAM"   
+           (is (= "INTERACTIVE-ID-WITH-PARAM"
 	         (karotz-request "INTERACTIVE-ID" "src/test/resources/ok-response.xml"))))))
 
-(deftest test-sign-in                           
+(deftest test-sign-in
    (testing "Signin should return valid id"
             (is (= "789"
                    (with-redefs [valid-id? (constantly false)
